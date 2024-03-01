dc6f9c3498dd185875f86d725bd131cc0c96dfab
whoamihack1ng <whoamihack1ng@gmail.com>
2024-03-01 23:01:39 +0500

    the project is finished

---
+++ app.js
@@ -0,0 +1,19 @@
+const url = "https://restcountries.com/v3.1/all";
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    nationFlag(data);
+  });
+ 
+function nationFlag(flags) {
+  flags.forEach(flag => {
+    // const countryName = flag.name.common;
+    // const flagUrl = flag.flags.svg;
+    // const card =`
+    //   <h1>${countryName}</h1>
+    //   <img src="${flagUrl}">
+    // `;
+    // document.body.innerHTML += card;
+  });
+}
